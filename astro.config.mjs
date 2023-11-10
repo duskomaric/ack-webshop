cebb1c2881ec23ff5b8cf99b99a2e1c6ed2c51c3
Dusko Maric <duskomaric86@gmail.com>
2023-11-10 18:09:59 +0100

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import alpine from "@astrojs/alpinejs";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ackrila.com',
   integrations: [alpine(), tailwind(), sitemap()],
-  output: 'server'
+  output: 'server',
+  adapter: vercel()
 });
